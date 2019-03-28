24d66ae479470a1a3be08488bdccf260cc14b987
Vlad Holubiev <golubev.vld@gmail.com>
2019-03-28 17:38:51 +0200

    feat: add config for typescript

---
+++ typescript.js
@@ -0,0 +1,25 @@
+const paddingLineBetweenStatements = require('./rules/padding-line-between-statements');
+
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  env: {
+    node: true,
+    jest: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'padding-line-between-statements': paddingLineBetweenStatements,
+    '@typescript-eslint/no-use-before-define': 0
+  }
+};
