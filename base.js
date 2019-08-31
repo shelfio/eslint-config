f1691dfe806e06229f6b2f08bb71c0ce21fde429
Vlad Holubiev <golubev.vld@gmail.com>
2019-09-01 02:07:59 +0300

    feat: add sort-imports eslint rule

--- base.js
+++ base.js
@@ -28,6 +28,15 @@ module.exports = {
       'error',
       {array: false, object: true},
       {enforceForRenamedProperties: false}
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+      }
     ]
   }
 };
--- typescript.js
+++ typescript.js
@@ -33,6 +33,15 @@ module.exports = {
       'error',
       {array: false, object: true},
       {enforceForRenamedProperties: false}
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+      }
     ]
   },
   overrides: [
