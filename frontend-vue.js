0f9ea5db8c14bd33fccedde20a7c9bf24b1bc70f
Marianna Milovanova <mmilovanova@kse.org.ua>
2019-11-05 18:14:01 +0200

    [CWC-545] Configure vue eslint according to latest parser options

--- frontend-vue.js
+++ frontend-vue.js
@@ -1,24 +1,18 @@
 const importOrder = require('./rules/import-order');
 
 module.exports = {
-  extends: ['@shelf/eslint-config', 'plugin:prettier/recommended'],
+  extends: ['@shelf/eslint-config', 'plugin:prettier/recommended', 'plugin:vue/essential'],
   plugins: ['vue', 'babel', 'jsx', 'import'],
   env: {
     browser: true
   },
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     ecmaFeatures: {
       jsx: true
     }
   },
-  settings: {
-    vue: {
-      pragma: 'h'
-    }
-  },
   rules: {
-    'import/order': importOrder,
-    'no-unused-vars': ['error', {vars: 'all', argsIgnorePattern: '^h$'}]
+    'import/order': importOrder
   }
 };
