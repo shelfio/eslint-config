668f43d7f972f5182b43255a0db7b44c5271d1f6
Vlad Holubiev <golubev.vld@gmail.com>
2021-01-02 21:25:51 +0200

    feat: allow require in next.config.js and draft.js

--- rules/overrides.js
+++ rules/overrides.js
@@ -3,13 +3,19 @@ module.exports = {
     files: ['**/*.test.ts'],
     // it's a bit annoying, there is no need to specify return types for test case function body it('...', fn)
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 0
-    }
+      '@typescript-eslint/explicit-function-return-type': 0,
+    },
   },
   allowRequireInConfigs: {
-    files: ['*wallaby.config.js', 'webpack.config.js', 'babel.config.js'],
+    files: [
+      '*wallaby.config.js',
+      'webpack.config.js',
+      'babel.config.js',
+      'next.config.js',
+      'draft.js',
+    ],
     rules: {
-      '@typescript-eslint/no-var-requires': 'off'
-    }
-  }
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  },
 };
