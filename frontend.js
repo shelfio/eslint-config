7799ffb8735d4838841dd379355c08b4434aea8a
Vlad Holubiev <golubev.vld@gmail.com>
2022-07-10 18:15:20 +0200

    Revert "refactor: remove base.js config"
    This reverts commit 2ac01e53edf4c5b9bc294665df03303f82567a48.

--- frontend.js
+++ frontend.js
@@ -1,9 +1,5 @@
 const env = require('./common/env');
-const paddingLineBetweenStatements = require('./rules/padding-line-between-statements.json');
-const jestRules = require('./rules/jest.json');
-const preferES6Features = require('./rules/prefer-es6.json');
-const importOrder = require('./rules/import-order.json');
-const sortImports = require('./rules/sort-imports.json');
+const baseConfig = require('./base');
 
 module.exports = {
   extends: [
@@ -33,20 +29,7 @@ module.exports = {
     },
   ],
   rules: {
-    'prettier/prettier': 'error',
-    ...paddingLineBetweenStatements,
-    ...jestRules,
-    ...preferES6Features,
-    'no-empty': ['error', {allowEmptyCatch: true}],
-    ...importOrder,
-    ...sortImports,
-    'comma-dangle': 'off',
-    camelcase: 'error',
-    eqeqeq: ['error', 'smart'],
-    'new-cap': 'error',
-    'no-extend-native': 'error',
-    'no-use-before-define': ['error', 'nofunc'],
-    'require-await': 'error',
+    ...baseConfig.rules,
     'no-console': 'error',
     'sonarjs/cognitive-complexity': ['error', 18],
     'multiline-comment-style': 'off',
