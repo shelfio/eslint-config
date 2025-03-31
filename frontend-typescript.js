4f7e1fed14775a0b5aae4fbeb2272f75e2bf36ea
Yaroslav Chapelskyi <chapelskyi.slavik@gmail.com>
2025-03-31 15:47:00 +0300

    refactor: move frontend rules directly to fe typescript config

--- frontend-typescript.js
+++ frontend-typescript.js
@@ -1,6 +1,6 @@
 import node from 'eslint-plugin-n';
 import testingLibrary from 'eslint-plugin-testing-library';
-import {fixupPluginRules} from '@eslint/compat';
+import {fixupConfigRules, fixupPluginRules} from '@eslint/compat';
 import tsParser from '@typescript-eslint/parser';
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
@@ -11,7 +11,15 @@ import overrides from './common/overrides.js';
 import youDontNeedLodash from './rules/you-dont-need-lodash.js';
 import typescriptRules from './rules/typescript.js';
 import consistentTypeImports from './rules/consistent-type-imports.js';
-import frontendConfig from './frontend.js';
+import sonarjs from 'eslint-plugin-sonarjs';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import baseConfig from './base.js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import _import from 'eslint-plugin-import';
+import globals from 'globals';
+import env from './common/env.js';
+import restrictedPackagesImport from './rules/restricted-packages-import.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -28,18 +36,38 @@ const testingLibraryReact = {
 export default [
   ...compat.extends('plugin:you-dont-need-lodash-underscore/compatible'),
   ...tsEslint.configs.recommended,
-  ...frontendConfig,
+  ...fixupConfigRules(compat.extends('plugin:react/recommended', 'plugin:react-hooks/recommended')),
+  sonarjs.configs.recommended,
+  eslintConfigPrettier,
+  ...baseConfig,
+  {
+    rules: {
+      ...restrictedPackagesImport,
+      'no-console': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+      'sonarjs/cognitive-complexity': ['error', 18],
+      '@stylistic/js/multiline-comment-style': 'off',
+      'no-unreachable': 'error',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
   testingLibraryReact,
   {
     plugins: {
+      react: fixupPluginRules(react),
+      'react-hooks': fixupPluginRules(reactHooks),
+      import: fixupPluginRules(_import),
       '@typescript-eslint': tsEslint.plugin,
       node,
       'testing-library': fixupPluginRules(testingLibrary),
     },
 
     languageOptions: {
+      globals: {
+        ...env,
+        ...globals.browser,
+      },
       parser: tsParser,
-
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
@@ -80,7 +108,6 @@ export default [
   overrides.noTSRulesWithJSON,
   {
     files: ['**/*.test.{ts,tsx,js}', '**/mocks.ts', '**/mock.js'],
-
     rules: {
       camelcase: 'off',
       'sonarjs/no-duplicate-string': 'off',
@@ -89,7 +116,6 @@ export default [
   },
   {
     files: ['**/*.styled.{ts,tsx}'],
-
     rules: {
       'sonarjs/no-nested-template-literals': 'off',
     },
